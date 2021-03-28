2d8c10273bfb66a011f4d941affc0d7abdb11cfb
janig7 <karoljanik17@gmail.com>
2021-03-28 11:09:01 +0200

    created getProducts function and type for cart item

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+// Components
+import Drawer from '@material-ui/core/Drawer';
+import LinearProgressBar from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import Badge from '@material-ui/core/Badge';
+// styles
+import { Wrapper } from './App.styles';
+// Types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  tittle: string;
+  ammount: number;
+};
+
+const getProducts = async (): Promise<CartItemType> =>
+  await (await fetch('https://fakestoreapi.com/products')).json();
+
+const App = () => {
+  return <div className='App'>Start</div>;
+};
+
+export default App;
