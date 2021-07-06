ddfe5e839145417e487f17a3bec9e946700cfa6e
karol-janik <karol.janik@wpengine.com>
2021-07-06 13:47:23 +0200

    fix

--- src/Item/Item.tsx
+++ src/Item/Item.tsx
@@ -15,7 +15,6 @@ const Item: React.FC<Props> = ({ item, handleAddToCart }) => (
       <h3>{item.tittle}</h3>
       <p>{item.description}</p>
       <h3>${item.price}</h3>
-      <h5>HELLO WORLD!</h5>
     </div>
     <Button onClick={() => handleAddToCart(item)}>Add to cart</Button>
   </Wrapper>
