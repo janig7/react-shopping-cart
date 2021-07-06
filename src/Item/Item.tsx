5c9f95c5f7a549bcb12e39f8871b4b8932b1714d
karol-janik <karol.janik@wpengine.com>
2021-07-06 12:36:04 +0200

    test commit

--- src/Item/Item.tsx
+++ src/Item/Item.tsx
@@ -1,13 +1,13 @@
-import Button from '@material-ui/core/Button';
+import Button from '@material-ui/core/Button'
 // Types
-import { CartItemType } from '../App';
+import { CartItemType } from '../App'
 // Styles
-import { Wrapper } from './Item.styles';
+import { Wrapper } from './Item.styles'
 
 type Props = {
-  item: CartItemType;
-  handleAddToCart: (clickedItem: CartItemType) => void;
-};
+  item: CartItemType
+  handleAddToCart: (clickedItem: CartItemType) => void
+}
 const Item: React.FC<Props> = ({ item, handleAddToCart }) => (
   <Wrapper>
     <img src={item.image} alt={item.tittle} />
@@ -15,9 +15,10 @@ const Item: React.FC<Props> = ({ item, handleAddToCart }) => (
       <h3>{item.tittle}</h3>
       <p>{item.description}</p>
       <h3>${item.price}</h3>
+      <h5>HELLO WORLD!</h5>
     </div>
     <Button onClick={() => handleAddToCart(item)}>Add to cart</Button>
   </Wrapper>
-);
+)
 
-export default Item;
+export default Item
